a4bd866534e8e9890699731d0c732a9ebd6083ba
Dave10 <32545447+DavidGiwojno@users.noreply.github.com>
2020-07-15 14:17:52 +0200

    Fixed the oh-my-zsh location

--- install.sh
+++ install.sh
@@ -24,8 +24,8 @@ fi
 echo "Installing oh-my-zsh..."
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.config/oh-my-zsh"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 
 echo "Done. Reload your terminal."
