03108325541cbef9f799c258222551501614c953
David Giwojno <davegiwo@gmail.com>
2021-11-05 15:10:39 +0100

    removed the prompt for the github name

--- install/git.sh
+++ install/git.sh
@@ -14,12 +14,16 @@ echo "--------------------------------------"
 echo -e "\n"
 
 read -rp "Github username [$defaultGithub] " github
-read -rp "Name [$defaultName] " name
 read -rp "Email [$defaultEmail] " email
 
 git config --global github.user "${github:-$defaultGithub}"
-git config --global user.name "${name:-$defaultName}"
+git config --global user.name "${github:-$defaultName}"
 git config --global user.email "${email:-$defaultEmail}"
+
+echo "----------------------------------------"
+echo "            Creating ssh key            "
+echo "----------------------------------------"
+echo -e "\n"
 
 ssh-keygen -t ed25519 -C "${email:-$defaultEmail}" -f "$HOME/.ssh/id_ed25519_github"
 echo "Added ssh key, now you can add it to your GitHub account"
