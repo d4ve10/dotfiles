d1b52b63135c814925d4d510a2423eb43b921545
David Giwojno <davegiwo@gmail.com>
2022-01-25 15:08:36 +0100

    better formatting

--- functions/git.sh
+++ functions/git.sh
@@ -8,7 +8,7 @@ if [[ "$defaultGithub" != "" ]]; then
 	return
 fi
 
-echo -ne "
+echo "
 --------------------------------------------------------------------
                            Setting up Git
 --------------------------------------------------------------------
@@ -21,16 +21,16 @@ git config --global github.user "${github:-$defaultGithub}"
 git config --global user.name "${github:-$defaultName}"
 git config --global user.email "${email:-$defaultEmail}"
 
-echo -ne "
+echo "
 --------------------------------------------------------------------
                           Creating SSH Key
 --------------------------------------------------------------------
 "
 
 ssh-keygen -t ed25519 -C "${email:-$defaultEmail}" -f "$HOME/.ssh/id_ed25519_github"
-echo -ne "
+echo -n "
 --------------------------------------------------------------------
                             Added SSH Key
-							  You can add it to your GitHub account
+                You can add it to your GitHub account
 --------------------------------------------------------------------
 "
